83b5f9da98a6af68b54275f52e4fa47238725d0d
Pavlo Moskvin <23moskvin@gmail.com>
2023-03-10 00:05:58 +0200

    I have fixed height and plus animation for DashBoard component

--- src/components/MainPage/DashboardInfo/DashboardInfo.jsx
+++ src/components/MainPage/DashboardInfo/DashboardInfo.jsx
@@ -1,11 +1,28 @@
 import style from './DashboardInfo.module.scss';
 import star from '../../../assets/icons/Star 4.svg';
-import { HiOutlineX } from 'react-icons/hi';
-import { useRef, useState } from 'react';
-
+import { motion } from 'framer-motion';
 const DashboardInfo = () => {
+  const textAnimation = {
+    hidden: {
+      y: 40,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      y: 0,
+      opacity: 1,
+    },
+  };
   return (
-    <div className={style.container}>
+    <motion.div
+      style={{ overflow: 'hidden' }}
+      variants={textAnimation}
+      initial="hidden"
+      whileInView="visible"
+      transition={{ duration: 0.7 }}
+      animate={{ opacity: 0, scale: 1 }}
+      viewport={{ amount: 0.3 }}
+      className={style.container}>
       <div className={style.text}>
         <p>
           <img src={star} alt="some icon" className={style.img1} />
@@ -15,7 +32,7 @@ const DashboardInfo = () => {
           <img src={star} alt="some icon" className={style.img2} />
         </p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
